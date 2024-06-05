a993a68aafe2fad7ba28ec0a097c3d5dc84046bd
Ramana <121662996+RamanaRaj7@users.noreply.github.com>
2024-06-06 01:50:48 +0530

    Update download.js
    fix in the number of commit logic which would greatly reduce the number of commits and color issue after running.
    Added comments to modify the number of commits and fix for errors which might occur while running the code

--- src/utils/download.js
+++ src/utils/download.js
@@ -7,7 +7,8 @@ function downloadFile(textSlice, graphSlice, yearSlice) {
   arrayToWrite.push(`mkdir github_painter`);
   arrayToWrite.push(`cd github_painter`);
   arrayToWrite.push(`git init`);
-  arrayToWrite.push(`git remote add origin ${textSlice.textFieldValue}`);
+  arrayToWrite.push(`git remote add origin ${textSlice.textFieldValue}`); 
+  arrayToWrite.push(`git pull origin main`);
   arrayToWrite.push(`touch foobar.txt`);
 
   function commitXTimes(x, date) {
@@ -22,13 +23,21 @@ function downloadFile(textSlice, graphSlice, yearSlice) {
   for (let i = 0; i < graphSlice.grid.length; i++) {
     // date starting at year day 1 + offset + i
     const date = new Date(yearSlice.selectedYear, 0, i + 1);
-    for (let j = 0; j < graphSlice.grid[i]; j++) {
-      // create `graphSlice.grid[i]` commits on `date`
-      commitXTimes(graphSlice.grid[i], date);
-    }
+    // create `graphSlice.grid[i]` commits on `date`
+    commitXTimes(graphSlice.grid[i], date);
   }
+  // to increase the number of commits modify this line 
+  // for example:commitXTimes(graphSlice.grid[i]*commitXTimes(graphSlice.grid[i], date); would square the initial number of commits
+  // for example:commitXTimes(graphSlice.grid[i]+2, date); would add 2 to the initial number of commits
+  // modify accoring to your needs
+  // initial commits would be
+  // space = 0
+  // a(green1 = '#0D4429';)=1
+  // s(green2 = '#016C31';)=2
+  // d(green3 = '#26A641';)=3
+  // f(green4 = '#39D353';)=4
 
-  arrayToWrite.push("git push origin main");
+  arrayToWrite.push("git push origin main --force");
   // yay, done!
   arrayToWrite.push("echo 'DONE!'");
 
