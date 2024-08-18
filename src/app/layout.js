172731d41ad39b14fbdc584370df3d7fab2a2632
Matthew Trent <matthew.rl.trent@gmail.com>
2024-08-17 17:22:37 -0700

    chore: adsense meta tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,17 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+import { Providers } from './../redux/provider'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'GitHub Painter',
+  description: 'Embrace your inner Picasso.',
+  icons: {
+    icon: '/favicon.ico',
+  },
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
