d6485080a82625a0fe748066b45db068fad651f5
Matthew Trent <matthew.rl.trent@gmail.com>
2024-12-16 18:17:50 -0800

    feat: metadata 2

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "GitHub Painter | Free Tool",
+  title: "GitHub Painter | Free Contribution Graph Customization Tool",
   description: "Customize your Contribution Graph · Create art with your GitHub contributions · Includes examples · Paint your commits · By Matthew Trent",
   icons: {
     icon: "/favicon.ico",
