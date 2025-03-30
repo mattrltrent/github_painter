fda2800b0a5b82f0091b53b22e770e7ed2d2e023
Eng. Xeeshan <xeeshaneng@gmail.com>
2025-03-30 17:26:19 +0500

    fix(layout): remove metadata export from client component and handle SEO manually via <Head>

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,21 +4,12 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { Providers } from "./../redux/provider";
 import Head from "next/head";
-import { useEffect } from "react"; // for keyboard event
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "GitHub Painter | Free Contribution Graph Customization Tool",
-  description:
-    "Customize your Contribution Graph · Create art with your GitHub contributions · Includes examples · Paint your commits · By Matthew Trent",
-  icons: {
-    icon: "/favicon.ico",
-  },
-};
-
 export default function RootLayout({ children }) {
-  // add global spacebar listener
+  // Add global spacebar listener
   useEffect(() => {
     const handleKeyDown = (e) => {
       if (e.code === "Space") {
@@ -34,9 +25,12 @@ export default function RootLayout({ children }) {
   return (
     <>
       <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <link rel="icon" href={metadata.icons.icon} />
+        <title>GitHub Painter | Free Contribution Graph Customization Tool</title>
+        <meta
+          name="description"
+          content="Customize your Contribution Graph · Create art with your GitHub contributions · Includes examples · Paint your commits · By Matthew Trent"
+        />
+        <link rel="icon" href="/favicon.ico" />
         <meta
           name="google-adsense-account"
           content="ca-pub-2826294656662096"
@@ -47,8 +41,9 @@ export default function RootLayout({ children }) {
           crossOrigin="anonymous"
         ></script>
       </Head>
+
       <html lang="en">
-        <body>
+        <body className={inter.className}>
           <Providers>{children}</Providers>
         </body>
       </html>
