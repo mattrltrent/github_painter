3d14055499a72f34b5fc35c2283f8a2b8795d01a
Matthew Trent <matthew.rl.trent@gmail.com>
2025-05-17 22:50:19 -0700

    fix: metadata

---
+++ src/app/page.metadata.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "GitHub Painter | Free Contribution Graph Customization Tool",
+  description:
+    "Customize your Contribution Graph · Create art with your GitHub contributions · Includes examples · Paint your commits · By Matthew Trent",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
