0cb7a32be6453b5a84183139ad73e324e2e74a2f
Matthew Trent <matthew.rl.trent@gmail.com>
2025-11-04 23:38:25 -0800

    .

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  assetPrefix: "https://github-painter.vercel.app",
   images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
 };
